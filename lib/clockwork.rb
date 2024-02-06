7a94e5ba184576996a67f2bbc174a431541abeb6
David Lau <david.kaimun.lau@gmail.com>
2024-02-06 14:00:08 +0100

    allow user to add a new project

--- lib/clockwork.rb
+++ lib/clockwork.rb
@@ -7,11 +7,15 @@ DB = Sequel.sqlite('db/database.db')
 require_relative 'models/project'
 require_relative 'models/session'
 require_relative 'option_parser'
+require_relative 'view'
 require_relative 'controller'
 
 # Parse options from command line
 options = OptionParser.parse(ARGV)
 
+# Create a new view
+view = View.new
+
 # Execute program using controller and passing options
-controller = Controller.new(options)
+controller = Controller.new(options, view)
 controller.execute
--- lib/controller.rb
+++ lib/controller.rb
@@ -1,10 +1,32 @@
+# frozen_string_literal: true
+
+# This class is responsible for executing the logic based on the options provided by the user.
 class Controller
-  def initialize(options)
+  def initialize(options, view)
     @options = options
+    @view = view
   end
 
   def execute
-    puts "Welcome to Clockwork! run 'clockwork.rb -h' for help." if @options.empty?
+    return @view.welcome if @options.empty?
+
+    return @view.too_many_options if @options.length > 1
+
     # TODO: Implement the logic to execute various actions based on the options
+    new_project if @options[:new]
+  end
+
+  private
+
+  def new_project
+    @view.display_message('Enter the name of the project:')
+    project_name = @view.ask_for_input
+    project = Project.new(name: project_name)
+    if project.valid?
+      project.save
+      @view.display_message("Project '#{project_name}' created successfully")
+    else
+      @view.display_error("Could not create project '#{project_name}': #{project.errors.full_messages.join(', ')}")
+    end
   end
 end
