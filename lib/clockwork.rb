f5fded8af8c5378df22e219ed0bc87b4e72599e7
David Lau <david.kaimun.lau@gmail.com>
2024-02-08 09:57:13 +0100

    add clock emoji to title

--- lib/clockwork.rb
+++ lib/clockwork.rb
@@ -24,4 +24,4 @@ sessions_controller = SessionsController.new(view)
 
 # Execute program using controller and passing options
 router = Router.new(options, view, projects_controller, sessions_controller)
-view.frame('Clock Work', :box, :magenta) { router.execute }
+view.frame('Clock Work ⏰', :box, :magenta) { router.execute }
